768f9056cf0bc6e2c8d14ccfd60e1e674cc18daa
Roman Bogorodskiy <bogorodskiy@gmail.com>
2016-11-05 21:39:55 +0300

    domain tests: add e1000 test

--- tests/base_test.py
+++ tests/base_test.py
@@ -9,6 +9,11 @@ logging.getLogger("paramiko").setLevel(logging.ERROR)
 
 class DomainGenericTest(unittest.TestCase,
                         base.DomainTest):
+    """Test for the most generic domain xml.
+
+    Uses virtio-net, so requires to have
+    ifconfig_vtnet0='DHCP' somewhere in /etc/rc.conf"""
+
     domain_xml_file = 'base.xml'
 
     def setUp(self):
@@ -22,3 +27,11 @@ class DomainGenericTest(unittest.TestCase,
         fixture_params["NAME"] = self.name_generate('dom')
         fixture_params["UUID"] = self.uuid()
         return fixture_params
+
+
+class DomainE1000Test(DomainGenericTest):
+    """Test for domain with e1000 nic
+
+    Requires ifconfig_em0='DHCP'"""
+
+    domain_xml_file = 'e1000.xml'
